c7ba422554d5c66861b9bddf4c08d9eefa98ffb0
mrx0 <alex.vilkov@bk.ru>
2018-08-21 07:12:40 +0300

    ...

--- app.js
+++ app.js
@@ -5,20 +5,43 @@ var phrases = {
     "world": "мир"
 };
 
+function PhraseError(message) {
+    this.message = message;
+    Error.captureStackTrace(this, PhraseError);
+}
+util.inherits(PhraseError, Error);
+PhraseError.prototype.name = 'PhraseError';
+
+function HttpError(status, message) {
+    this.status = status;
+    this.message = message;
+    Error.captureStackTrace(this, HttpError);
+}
+util.inherits(HttpError, Error);
+HttpError.prototype.name = 'HttpError';
+
 function getPhrases (name){
     if (!phrases[name]){
-        throw new Error("Нет такой фразы: " + name);
+        throw new PhraseError(500, "Нет такой фразы: " + name);
     }
     return phrases[name];
 }
 
 function makePage(url){
     if (url != 'index.html'){
-        throw new Error("Нет такой страницы");
+        throw new HttpError(404, "Нет такой страницы");
     }
-    return util.format("%s, %s!", getPhrases("Hello"), getPhrases("world"));
+    return util.format("%s, %s!", getPhrases("1Hello"), getPhrases("world"));
 }
 
-var page = makePage('index.html');
+try {
+    var page = makePage('index.html');
+    console.log(page);
+} catch (e){
+    if (e instanceof HttpError){
+        console.log(e.status, e.message);
+    }else{
+        console.log("Ошибка %s\n сообщение: %s\n стек: %s", e.name, e.message, e.stack);
+    }
+}
 
-console.log(page);
